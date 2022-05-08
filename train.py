cfebaa36b22f418c4ac89fbfe5592ceeb56ffe77
Leello Tadesse Dadi <leello.tadesse@gmail.com>
2022-05-08 21:21:43 +0200

    adds bug fixes

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
-from jax import grad
+from jax import grad, tree_map
 import numpy as np
 import jax.numpy as jnp
 from jax.config import config
+
 config.update("jax_enable_x64", True)
 
 import matplotlib.pyplot as plt
-from utils import compute_gradient_norm, create_params, one_hot
+from utils import compute_distance, compute_gradient_norm, create_params, one_hot
 from model import accuracy, loss
 from dataloader import MNIST, FashionMNIST, NumpyLoader, FlattenAndCast
 from optimizers import SGD, AdaGrad, SpiderBoost, AdaSpider
@@ -13,12 +14,12 @@ import wandb
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', default=128, type=int)
-parser.add_argument('--step_size', default=0.01, type=float)
-parser.add_argument('--eta', default=0.01, type=float)
-parser.add_argument('--epsilon', default=1e-4, type=float)
-parser.add_argument('--epochs', default=10, type=int)
-parser.add_argument('--optimizer', default='SGD', type=str)
+parser.add_argument("--batch_size", default=128, type=int)
+parser.add_argument("--step_size", default=0.01, type=float)
+parser.add_argument("--eta", default=0.01, type=float)
+parser.add_argument("--epsilon", default=1e-4, type=float)
+parser.add_argument("--epochs", default=10, type=int)
+parser.add_argument("--optimizer", default="SGD", type=str)
 
 args = parser.parse_args()
 
@@ -28,8 +29,13 @@ step_size = args.step_size
 eta = args.eta
 epsilon = args.epsilon
 T = args.epochs
-optimizers = {'SGD': SGD, 'AdaSpider': AdaSpider, 'AdaGrad': AdaGrad}
-optimizer_params = {'SGD': {'step_size' : step_size}, 'AdaSpider': {'n': 50000}, 'AdaGrad': {'eta':eta, 'epsilon': epsilon}}
+sgd_epochs = 4
+optimizers = {"SGD": SGD, "AdaSpider": AdaSpider, "AdaGrad": AdaGrad}
+optimizer_params = {
+    "SGD": {"step_size": step_size},
+    "AdaSpider": {"n": 60000, "eta": eta},
+    "AdaGrad": {"eta": eta, "epsilon": epsilon},
+}
 algorithm = optimizers[args.optimizer]
 optimizer = algorithm(**optimizer_params[args.optimizer])
 ######
@@ -37,15 +43,16 @@ optimizer = algorithm(**optimizer_params[args.optimizer])
 layer_sizes = [28 * 28, 512, 512, 10]
 num_classes = 10
 
-logger = wandb.init(project="AdaSpider", name=optimizer.__str__(),
-           config={"batch_size": batch_size, "epochs": T, "layer_sizes": layer_sizes})
+logger = wandb.init(
+    project="AdaSpider",
+    name=optimizer.__str__(),
+    config={"batch_size": batch_size, "epochs": T, "layer_sizes": layer_sizes},
+)
 wandb.config.update(args)
 
 dataset = FashionMNIST("/tmp/mnist/", download=True, transform=FlattenAndCast())
-training_generator = NumpyLoader(dataset, batch_size=batch_size, num_workers=8)
-train_images = np.array(dataset.data).reshape(
-    len(dataset.data), -1
-)
+training_generator = NumpyLoader(dataset, batch_size=batch_size, num_workers=0)
+train_images = np.array(dataset.data).reshape(len(dataset.data), -1)
 train_labels = one_hot(np.array(dataset.targets), num_classes)
 dataset_test = FashionMNIST("/tmp/mnist/", download=True, train=False)
 test_images = jnp.array(
@@ -56,23 +63,30 @@ test_labels = one_hot(np.array(dataset_test.targets), num_classes)
 
 
 params = create_params(layer_sizes)
+starting_params = tree_map(lambda x: x.copy(), params)
 state = optimizer.create_state(params)
 loss_lst = []
+
 for epoch in range(T):
     state = algorithm.on_epoch_state_update(params, state, (train_images, train_labels))
-    for x, y in training_generator:
+    for (idx, (x, y)) in enumerate(training_generator):
         y = one_hot(y, num_classes)
-        params, state = algorithm.update(params, state, (x,y))
+        if idx > 0 or args.optimizer != 'AdaSpider':
+            state = algorithm.on_step_state_update(params, state, (x,y))
+        params, state = algorithm.update(params, state, (x, y))
         batch_loss = loss(params, x, y)
         logger.log({"loss": batch_loss})
+        if args.optimizer == 'AdaSpider':
+            logger.log({"step_size": state["step_size"]})
 
     train_acc = accuracy(params, train_images, train_labels)
     test_acc = accuracy(params, test_images, test_labels)
     gradient_norm = compute_gradient_norm(params, train_images, train_labels)
+    distance_from_init = compute_distance(params, starting_params)
     logger.log({"train_acc": train_acc})
     logger.log({"test_acc": test_acc})
-    logger.log({'grad_norm': gradient_norm})
-
+    logger.log({"grad_norm": gradient_norm})
+    logger.log({"distance_from_init": distance_from_init})
     print("#### Epoch {}".format(epoch))
     print("Training set accuracy {}".format(train_acc))
     print("Test set accuracy {}".format(test_acc))
--- utils.py
+++ utils.py
@@ -1,35 +1,34 @@
 import numpy as np
 import jax.numpy as jnp
 import jax
-from jax import jit, grad
+from jax import jit, grad, tree_map
+from jax.tree_util import tree_reduce
 from functools import partial
 from model import loss
 
+
 def create_params(layer_widths):
-  params = []
-  for n_in, n_out in zip(layer_widths[:-1], layer_widths[1:]):
-    params.append(
-        dict(weights=0.01*np.random.normal(size=(n_in, n_out)),
-             biases=0.01*np.random.normal(size=(n_out,))
+    params = []
+    for n_in, n_out in zip(layer_widths[:-1], layer_widths[1:]):
+        params.append(
+            dict(
+                weights=0.0001 * jnp.array(np.random.normal(size=(n_in, n_out))),
+                biases=0.0001 * jnp.array(np.random.normal(size=(n_out,))),
             )
-    )
-  return params
+        )
+    return params
 
-@partial(jit, static_argnames=['k', 'dtype'])
+
+@partial(jit, static_argnames=["k", "dtype"])
 def one_hot(x, k, dtype=jnp.float32):
-  return jnp.array(x[:, None] == jnp.arange(k), dtype)
+    return jnp.array(x[:, None] == jnp.arange(k), dtype)
 
-def create_state(layer_widths):
-  state = []
-  for n_in, n_out in zip(layer_widths[:-1], layer_widths[1:]):
-    state.append(
-        dict(weights=np.zeros((n_in, n_out)) + 1e-3,
-             biases=np.zeros(n_out) + 1e-3
-            )
-    )
-  return state
-
+@jit
 def compute_gradient_norm(params, x, y):
-  grads = grad(loss)(params, x, y)
-  norms = jax.tree_map(lambda v: jnp.sum(v*v), grads)
-  return jax.tree_util.tree_reduce(lambda a, b: a + b, norms, 0.0)
+    grads = grad(loss)(params, x, y)
+    norms = jax.tree_map(lambda v: jnp.sum(v * v), grads)
+    return jax.tree_util.tree_reduce(lambda a, b: a + b, norms, 0.0)
+
+def compute_distance(params, starting_params):
+  distances = tree_map(lambda a, b: jnp.linalg.norm(a - b)**2, params, starting_params)
+  return jax.tree_util.tree_reduce(lambda a, b: a+b, distances, 0.0)
