38ed7c692feae1243f0f771df5e4fde73725aa05
Leello Tadesse Dadi <leello.tadesse@gmail.com>
2022-05-26 16:01:40 +0200

    removes jitts

--- model.py
+++ model.py
@@ -15,14 +15,13 @@ def net(x, is_training=True):
 net = hk.transform_with_state(net)
 
 
-@partial(jit, static_argnames='is_training')
 def loss(params, net_state, x, y, is_training=True):
     out, net_state = net.apply(params, net_state, None, x, is_training=is_training)
     logits = out - logsumexp(out, axis=1).reshape(-1, 1)
     return -jnp.mean(y * logits), net_state
 
 
-@jit
+
 def accuracy(params, net_state, x, y):
     true_class = jnp.argmax(y, axis=1)
     predicted_class = jnp.argmax(net.apply(params, net_state, None, x, False)[0], axis=1)
